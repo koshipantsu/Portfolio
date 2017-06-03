61183ef8e0f269ded8b46b8b6340231238275f3b
koshipantsu <giganticmagnum@gmail.com>
2017-06-03 23:40:54 +0900

    完成

--- assets/js/top.js
+++ assets/js/top.js
@@ -5,7 +5,7 @@ $(function(){
     var speed = 500;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
+    var position = $(target).offset().top;
     $("html, body").animate({scrollTop:position}, 1000, "swing");
     return false;
   });
